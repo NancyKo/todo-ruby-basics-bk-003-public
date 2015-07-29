257495269616ad6ad54f9d19d13ee783f3f027fa
NancyKo <konancys@gmail.com>
2015-07-29 08:50:45 -0400

    methods 1-4 complete

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,21 +1,21 @@
 def division(num1, num2)
- 
+ sum = num1 / num2
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(phrase)
+  phrase
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-
+  puts "Nice!"
 end
 
 def last_evaluated_value
