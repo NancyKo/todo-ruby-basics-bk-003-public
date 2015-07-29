6f8bc982edb22bddcd628ef7b9de9d3f6eec80b8
NancyKo <konancys@gmail.com>
2015-07-29 09:15:59 -0400

    all methods filled, except the last one

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -15,13 +15,20 @@ def greeting(greeting, name)
 end
 
 def return_a_value
-  puts "Nice!"
+  value = "Nice"
+  return value
 end
 
 def last_evaluated_value
-
+  value = "expert"
+  return value
 end
 
-def pizza_party
-
+def pizza_party(*cheese)
+  return cheese
 end
+
+def cheese
+  puts "cheese"
+end 
+
